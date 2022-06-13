9157fc9b2dec82b5ee253850b1494d84ab73d054
watchakorn-18k <porton2559@gmail.com>
2022-06-13 23:28:56 +0700

    Update add to json and validate

--- app.py
+++ app.py
@@ -39,51 +39,126 @@ class AppRegister:
         self.page = page
         self.page.horizontal_alignment = 'center'
         self.page.vertical_alignment = 'start'
+        self.Is_has_validate_name = {'status_name':0,'status_surname':0}
+        self.Is_has_name_surname = 0
+        self.Is_has_session = 0
+
+        self.create_form()
+        self.btn()
+        if self.Is_has_session == 1:
+            self.Submit.disabled = True
+
+    def create_form(self):
         self.NameInput = TextField(label='ชื่อ',hint_text='กรอกชื่อจริง',width=300)
         self.SurnameInput = TextField(label='นามสกุล',hint_text='กรอกนามสกุล',width=300)
         self.AgeInput = TextField(label='อายุ',hint_text='อายุ',width=300,keyboard_type='number',value = 0)
         self.TextHeader = Text('สมัครสมาชิก',style="displaySmall")
-        self.Submit = FilledButton('ลงทะเบียน',height=50,width=200)
-        self.Submit.on_click = self.submit
-        self.BackMain = FilledButton('กลับหน้าหลัก',height=50,width=200)
-        self.BackMain.on_click = self.back_main
+
         self.Container1 = Container(content=self.TextHeader,margin=10,)
         self.Container2 = Container(content=self.NameInput,margin=5,padding=padding.only(left=30,right=30))
         self.Container3 = Container(content=self.SurnameInput,margin=5,padding=padding.only(left=30,right=30))
         self.Container4 = Container(content=self.AgeInput,margin=5,padding=padding.only(left=30,right=30))
-        self.Container5 = Container(content=self.Submit,margin=5,padding=padding.only(left=30,right=30))
-        self.Container6 = Container(content=self.BackMain,margin=5,padding=padding.only(left=30,right=30))
-        self.InputList = [self.Container1,self.Container2,self.Container3,self.Container4,self.Container5,self.Container6]
+        self.InputList = [self.Container1,self.Container2,self.Container3,self.Container4]
         for i in self.InputList:
             self.page.add(i)
         self.page.update()
-
+    
+    def btn(self):
+        self.Submit = FilledButton('ลงทะเบียน',height=50,width=200)
+        self.Submit.on_click = self.submit
+        self.BackMain = FilledButton('กลับหน้าหลัก',height=50,width=200)
+        self.BackMain.on_click = self.back_main
+        self.Container5 = Container(content=self.Submit,margin=5,padding=padding.only(left=30,right=30))
+        self.Container6 = Container(content=self.BackMain,margin=5,padding=padding.only(left=30,right=30))
+        self.BtnList = [self.Container5,self.Container6]
+        
+        for i in self.BtnList:
+            self.page.add(i)
+        self.page.update()
 
     def back_main(self,event):
         """
         กลับไปหน้าหลัก
         """
+        if self.Is_has_session == 1:
+            self.page.remove_at(4)
+        if self.Is_has_validate_name['status_name'] == 1:
+            self.page.remove_at(2)
+        if self.Is_has_validate_name['status_surname'] == 1:
+            self.page.remove_at(3)
+
+        self.pop_form()
+        self.pop_btn()
+        AppMain(self.page)
+    def pop_form(self):
         for i in self.InputList:
             self.page.controls.pop()
-        AppMain(self.page)
+    def pop_btn(self):
+        for i in self.BtnList:
+            self.page.controls.pop()
 
     def submit(self,event):
         import json
         """
         ส่งข้อมูลลงฐานข้อมูล
         """
-        data = {
+        data = [{
             "id":self.page.session_id,
             "name":self.NameInput.value,
             "surname":self.SurnameInput.value,
             "age":self.AgeInput.value
-        }
-        with open('data.json','r') as f:
-            data_json = json.load(f)
-        data_json.append(data)
-        with open('data.json','w') as f:
-            json.dump(data_json,f)
-            
+        }]
+        if self.NameInput.value != "" and self.SurnameInput.value != "":
+            with open('data.json','r',encoding="utf-8") as f:
+                data_json = json.loads(f.read())
+            if len(data_json) >= 0:
+                if self.page.session_id not in [i['id'] for i in data_json] :
+                    if self.NameInput.value not in [i['name'] for i in data_json] or self.SurnameInput.value not in [i['surname'] for i in data_json]:
+                        data_json.append(data[0])
+                        with open('data.json','w',encoding="utf-8") as f:
+                            f.write(json.dumps(data_json,indent=4,ensure_ascii=False))
+                    else:
+                        if self.Is_has_name_surname != 1:
+                            if self.Is_has_validate_name['status_name'] == 1:
+                                self.page.remove_at(2)
+                            if self.Is_has_validate_name['status_surname'] == 1:
+                                self.page.remove_at(3)
+                            if self.Is_has_session == 1:
+                                self.page.remove_at(4)
+                            self.page.insert(4,Text('ชื่อหรือนามสกุลเคยลงทะเบียนไว้แล้ว',style="bodySmall",text_align='start'))
+                            self.Is_has_name_surname = 1
+                else:
+                    if self.Is_has_session != 1:
+                        if self.Is_has_validate_name['status_name'] == 1:
+                            self.page.remove_at(2)
+                        if self.Is_has_validate_name['status_surname'] == 1:
+                            self.page.remove_at(3)
+                        if self.Is_has_name_surname == 1:
+                            self.page.remove_at(4)
+                    
+                        self.Submit.disabled = True
+                        self.page.insert(4,Text('คุณได้ลงทะเบียนไปแล้ว',style="bodySmall",text_align='start',))
+                        self.Is_has_session = 1
+                        self.Is_has_name_surname = 1
+
+        else:
+            if self.Is_has_validate_name['status_name'] != 1:
+                self.page.insert(2,Text('กรุณากรอกข้อมูลชื่อให้ครบถ้วน',style="bodySmall",text_align='start'))
+                self.Is_has_validate_name['status_name'] = 1
+            if self.Is_has_validate_name['status_surname'] != 1:
+                self.page.insert(4,Text('กรุณากรอกข้อมูลนามสกุลให้ครบถ้วน',style="bodySmall",text_align='start'))
+                self.Is_has_validate_name['status_surname'] = 1
+        # try:
+        #     with open('data.json','r',encoding="utf-8") as f:
+        #         data_json = json.load(f)
+        #     if len(data_json) >= 1:
+        #         with open('data.json','w',encoding="utf-8") as f:
+        #             data_json.append(data[0])
+        #             json.dump(data_json,f,ensure_ascii=False)
+        # except:
+        #     with open('data.json','w',encoding="utf-8") as f:
+        #         json.dump(data,f,ensure_ascii=False)
+                    
         # print(self.page.session_id)
         # print(self.NameInput.value)
         # print(self.SurnameInput.value)
@@ -118,4 +193,4 @@ def main(page: Page):
     page.update()
 
 
-flet.app(target=main,port=25648,)
+flet.app(target=main,port=25648,view=flet.WEB_BROWSER)
